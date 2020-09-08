5cdf971eedd51d51a470be8e189528cf1e808040
Ross Vrana-Godwin <rossvg@hotmail.ca>
2020-09-08 15:42:01 -0700

    Added batch file to run script
    This batch file contains instructions to run the script in a minimized command window. Requires the python.exe path followed by the python script to be run file path

---
+++ run_python_script.bat
@@ -0,0 +1,3 @@
+if not DEFINED IS_MINIMIZED set IS_MINIMIZED=1 && start "" /min "%~dpnx0" %* && exit
+"C:\Users\Ross\AppData\Local\Programs\Python\Python38-32\python.exe" "C:\Users\Ross\Desktop\PythonLearning\Notif\JobNotification.py"
+exit
